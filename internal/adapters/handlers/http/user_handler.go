e78f093755baecb9717445a84faa76b08c3a34d8
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-02-12 12:09:57 +0500

    Adjustments to a new dev environment

--- internal/adapters/handlers/http/user_handler.go
+++ internal/adapters/handlers/http/user_handler.go
@@ -71,7 +71,8 @@ func (h *userHandler) Register(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
+	// w.WriteHeader(http.StatusCreated)
+	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(response)
 }
 
